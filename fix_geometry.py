2fa4a1c6ccf539682cef6132467c5c5fc3f603ee
ttlbyte <sqruan@smail.nju.edu.cn>
2015-11-24 12:17:04 +0800

    fix the bug adding constrain info after the lattice line

--- fix_geometry.py
+++ fix_geometry.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
-import sys
+import sys, os
 fo=open('geometry.in','r');
 fw=open('output','w');
 line=fo.readline();
 while(len(line)>2):
-#	if (((float(line.split()[3])-0.024)>0.000001)and((float(line.split()[3])-0.21)<0.0001)):
-	if ((float(line.split()[3])-5.6)<0.00001):
+	if len(line.split())<5:
+		fw.write(line);
+	elif ((float(line.split()[3])-5.6)<0.00001):
 		fw.write(line);
 		fw.write("constrain_relaxation .true.\n");
 	else:
@@ -13,3 +14,5 @@ while(len(line)>2):
 	line=fo.readline();
 fw.close();
 fo.close();
+
+os.system('mv output geometry.in')
